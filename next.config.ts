c600762947b37f9a12b2482da17c1d03193a575f
fahrilfernanda <fahrilfernanda28@gmail.com>
2026-06-02 16:54:12 +0700

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/data-keuangan",
+  assetPrefix: "/data-keuangan/",
 };
 
 export default nextConfig;
