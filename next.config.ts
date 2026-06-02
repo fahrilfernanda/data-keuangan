12a85e0827015ca9d177e99c35a35c01a06490ac
fahrilfernanda <fahrilfernanda28@gmail.com>
2026-06-02 20:56:04 +0700

    Fix Next.js config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  // GitHub Pages project repo
   basePath: "/data-keuangan",
   assetPrefix: "/data-keuangan/",
+
+  // penting untuk static export biar tidak error saat build
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
